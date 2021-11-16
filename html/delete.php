3e7306d82c1db2b620bc9faffc6b61a91bead2d8
spencer.warren.19 <spencer.warren.19@cnu.edu>
2021-11-16 14:28:20 -0500

    Write delete file

--- html/delete.php
+++ html/delete.php
@@ -3,12 +3,24 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="formating.css">
 </head>
 <body>
+    <form action="delete.php" method="post">
+    <input type = "text" name = "ID" id = "ID"><br>
+    <input type ="submit" name = "Delete Item">
+    </form>
 <?php
     require "connect.php";
-    $query = "SELECT * FROM ?";
+    $ID = $_POST["ID"];
+    $query = "DELETE FROM ? WHERE ID=".$ID;
     $res=mysqli_query($conn, $query);
+    if($res) {
+        echo"<p>".$ID." was successfully deleted</p>";
+    }
+    else{
+        echo"<p>".$ID." failed to be deleted</p>";
+    }
 ?>
 </body>
 </html>
